ec35f46d26913594eb0fd37687392f260372a3f3
HenriqueSD <hsd.foz@gmail.com>
2020-01-10 08:16:06 -0300

    RXJS - Observables

--- RXJS/src/app/app.component.ts
+++ RXJS/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: "app-root",
@@ -42,13 +43,32 @@ export class AppComponent implements OnInit {
     });
   }
 
+  minhaObservable(nome: string): Observable<string> {
+    return new Observable(subscriber => {
+      if (nome === 'Henrique') {
+        subscriber.next('Olá! ' + nome);
+        subscriber.next('Olá de novo! ' + nome);
+        setTimeout(() => {
+          subscriber.next('Resposta com delay ' + nome);
+        }, 5000);
+      } else {
+        subscriber.error('Ops! Deu erro!');
+      }
+    });
+  }
+
   ngOnInit(): void {
     // this.minhaPromise('Henrique')
     // .then(result => console.log(result));
 
-    this.minhaPromise('Jose')
-    .then(result => console.log(result))
-    .catch(erro => console.log(erro));
+    // this.minhaPromise('Jose')
+    // .then(result => console.log(result))
+    // .catch(erro => console.log(erro));
+
+    this.minhaObservable('Henrique')
+      .subscribe(
+        result => console.log(result),
+        erro => console.log(erro));
   }
 
 }
