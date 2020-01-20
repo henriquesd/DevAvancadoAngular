6eb9e80187f198a1c0cf8989e8bc0fbb170bc3ab
henrique <hsd.foz@gmail.com>
2020-01-20 16:04:11 -0300

    Módulos compartilhados

--- Forms/src/app/app.module.ts
+++ Forms/src/app/app.module.ts
@@ -9,19 +9,14 @@ import { TextMask } from 'ng-brazil';
 import { CustomFormsModule } from 'ng2-validation'
 
 import { AppComponent } from './app.component';
-import { MenuComponent } from './navegacao/menu/menu.component';
-import { HomeComponent } from './navegacao/home/home.component';
-import { FooterComponent } from './navegacao/footer/footer.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { rootRouterConfig } from './app.routes';
 import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
+import { NavegacaoModule } from './navegacao/navegacao.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
-    HomeComponent,
-    FooterComponent,
     SobreComponent,
     CadastroComponent
   ],
@@ -29,6 +24,7 @@ import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.compo
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    NavegacaoModule,
     TextMask.TextMaskModule,
     NgBrazil,
     CustomFormsModule,
