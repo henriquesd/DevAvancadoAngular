6a42509dda902654f0374bcd89bd3ef9ee1e3dc4
henriquesd <hsd.foz@gmail.com>
2020-04-20 20:37:41 +0200

    NgZones

--- Forms/src/app/demos/bar-di-zones/bar.component.ts
+++ Forms/src/app/demos/bar-di-zones/bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject, Injector } from "@angular/core";
+import { Component, OnInit, Inject, Injector, NgZone } from "@angular/core";
 import { BarServices, BarServicesMock, BarFactory, BebidaService } from './bar.service';
 import { BarUnidadeConfig, BAR_UNIDADE_CONFIG } from './bar.config';
 import { HttpClient } from '@angular/common/http';
@@ -34,7 +34,8 @@ export class BarComponent implements OnInit {
         private barServices: BarServices,
         @Inject('ConfigManualUnidade') private ApiConfigManual: BarUnidadeConfig,
         @Inject(BAR_UNIDADE_CONFIG) private ApiConfig: BarUnidadeConfig,
-        private bebidaService : BebidaService
+        private bebidaService : BebidaService,
+        private ngZone: NgZone
         ) { }
 
     ngOnInit(): void {
@@ -45,5 +46,40 @@ export class BarComponent implements OnInit {
         
         this.barBebida2 = this.bebidaService.obterBebidas();
     }
+
+    public progress: number = 0;
+    public label: string;
+
+    processWithinAngularZone() {
+        this.label = 'dentro';
+        this.progress = 0;
+        this._increaseProgress(() => console.log('Finalizado por dentro do Angular!'));
+    }
+
+    processOutsideOfAngularZone() {
+        this.label = 'fora';
+        this.progress = 0;
+        this.ngZone.runOutsideAngular(() => {
+        this._increaseProgress(() => {
+            this.ngZone.run(() => { console.log('Finalizado fora!'); });
+        });
+      });
+    }
     
+    // Ele passa um callback, que é um método void (doneCallback),
+    // e esse callback só vai ser chamado quando o progresso for 100,
+    // enquanto for menor que 100 ele ta continuando setando o _increaseProgress,
+    // quando ele finalizar ele chama o método callback (doneCallback), que no caso
+    // ele vai trazer esse processamento para dentro do Angular informando então que
+    // ele chegou em 100% (this.ngZone.run(() => { console.log('Finalizado fora!'); }););
+    _increaseProgress(doneCallback: () => void) {
+        this.progress += 1;
+        console.log(`Progresso atual: ${this.progress}%`);
+    
+        if (this.progress < 100) {
+          window.setTimeout(() => this._increaseProgress(doneCallback), 10);
+        } else {
+          doneCallback();
+        }
+      }
 }
